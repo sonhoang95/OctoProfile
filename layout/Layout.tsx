6e0672739e33eec481ae3da7ad40bd06b627c8c7
Son Nguyen <sonhoang95@gmail.com>
2022-03-07 10:38:36 -0600

    add file structure and layout component

---
+++ layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from '../components';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="bg-black min-h-screen flex flex-col text-gray-100">
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
