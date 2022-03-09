7dba59b660d04b07356fbde1282e5fbfa3ab6912
Son Nguyen <sonhoang95@gmail.com>
2022-03-09 13:32:41 -0600

    added bar and column charts

--- components/Chart/DynamicChartWithNoSSR.tsx
+++ components/Chart/DynamicChartWithNoSSR.tsx
@@ -11,3 +11,11 @@ export const DynamicPie2D = dynamic(() => import('./Pie2D'), {
 export const Doughnut2D = dynamic(() => import('./Doughnut2D'), {
   ssr: false,
 });
+
+export const Column2D = dynamic(() => import('./Column2D'), {
+  ssr: false,
+});
+
+export const Bar2D = dynamic(() => import('./Bar2D'), {
+  ssr: false,
+});
--- components/Repos.tsx
+++ components/Repos.tsx
@@ -1,26 +1,43 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-import { Doughnut2D, DynamicPie2D } from './Chart/DynamicChartWithNoSSR';
+import { Language } from '../type';
+import {
+  Bar2D,
+  Column2D,
+  Doughnut2D,
+  DynamicPie2D,
+} from './Chart/DynamicChartWithNoSSR';
 
 const Repos = () => {
   const { repos } = useContext(AppContext);
-  let languages: any = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
     if (!language) return total;
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  const mostUsedLanguages: any = Object.values(languages)
     .sort((a: { value: number }, b: { value: number }) => {
       return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  // most stars per languages
+  const mostPopular: any = Object.values(languages)
+    .sort((a: { stars: number }, b: { stars: number }) => {
+      return b.stars - a.stars;
+    })
+    .map((item: any) => {
+      return { ...item, value: item.stars };
     })
     .slice(0, 5);
 
@@ -43,12 +60,12 @@ const Repos = () => {
     <section>
       <div className="max-w-[1200px] mx-auto grid lg:grid-cols-[2fr_3fr] gap-6 px-6 lg:px-0 mb-3 lg:mb-8">
         {/* <DynamicExampleChart data={chartData} /> */}
-        <DynamicPie2D data={languages} />
-        <div></div>
+        <DynamicPie2D data={mostUsedLanguages} />
+        <Column2D data={chartData} />
       </div>
       <div className="max-w-[1200px] mx-auto grid lg:grid-cols-[2fr_3fr] gap-6 px-6 lg:px-0 mb-8">
-        <Doughnut2D data={chartData} />
-        <div></div>
+        <Doughnut2D data={mostPopular} />
+        <Bar2D data={chartData} />
       </div>
     </section>
   );
